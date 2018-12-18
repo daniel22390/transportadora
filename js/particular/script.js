d205f552e81dd0466f49eb4a05648e56f8c3d154
Daniel Gomes de Oliveira <daniel22390@hotmail.com>
2018-12-17 23:31:31 -0200

    initial commit

---
+++ js/particular/script.js
@@ -0,0 +1,53 @@
+$.Geral = {};
+
+$.Geral.events = function(){
+  $(".menu_i").click(function(){
+    if($('.sidenav').hasClass('toggle-class')){
+      $(".sidenav").removeClass('toggle-class');
+      $(".sidenav").addClass('notoggle-class');
+      $('.content-elements > div').css('width', "calc(100% - 120px)");
+      $('.content-elements > div').css('marginLeft', "120px");
+    }
+    else{
+      $(".sidenav").addClass('toggle-class');
+      $(".sidenav").removeClass('notoggle-class');
+      $('.content-elements > div').css('width', "100%");
+      $('.content-elements > div').css('marginLeft', "0px");
+    }
+  });
+
+  $('body').on('click', '.side li', function(ev){
+    $('.content-elements > div').hide();
+    $('.sidenav > li').removeClass("selected");
+    if($(this).hasClass('home')){
+      $('.home-body').show();
+      $('.home').addClass('selected');
+    }
+    else if($(this).hasClass('pacotes')){
+      $('.pacotes-body').show();
+      $('.pacotes').addClass('selected');
+    }
+    else if($(this).hasClass('rotas')){
+      $('.rotas-body').show();
+      $('.rotas').addClass('selected');
+    }
+    else if($(this).hasClass('gerencia')){
+      $('.gerencia-body').show();
+      $('.gerencia').addClass('selected');
+    }
+  });
+
+  $('body').on('click', '.filtro_produto', function(ev){
+    $('.pesquisa').remove();
+    $('table tbody').prepend('<tr class="pesquisa"><td><input type="number" class="form-control"></td><td><input type="text" class="form-control"></td><td><input type="text" class="form-control"></td><td><input type="text" class="form-control"></td><td><input type="text" class="form-control"></td><td><input type="text" class="form-control"></td><td><input type="text" class="form-control"></td></tr>');
+  });
+  $('body').on('click', function(ev){
+    if(!$(event.target).closest('.pesquisa').length && !$(event.target).closest('.filtro_produto').length) {
+      $('.pesquisa').remove();
+    }
+  });
+}
+
+$(document).ready(function(){
+  $.Geral.events();
+});
